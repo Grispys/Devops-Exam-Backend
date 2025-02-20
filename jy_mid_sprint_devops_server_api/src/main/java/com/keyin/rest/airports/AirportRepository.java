f5beb36bac3212bfca1d322163075d53553f7c00
Joshua Youden <Joshua.Youden@keyin.com>
2025-02-20 19:00:53 -0330

    Added code to airport files

--- jy_mid_sprint_devops_server_api/src/main/java/com/keyin/rest/airports/AirportRepository.java
+++ jy_mid_sprint_devops_server_api/src/main/java/com/keyin/rest/airports/AirportRepository.java
@@ -1,3 +1,10 @@
 package com.keyin.rest.airports;
 
-public class AirportRepository {}
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AirportRepository extends CrudRepository<Airport, Long> {
+    public Airport findByName(String name);
+    public Airport findByCode(String code);
+}
