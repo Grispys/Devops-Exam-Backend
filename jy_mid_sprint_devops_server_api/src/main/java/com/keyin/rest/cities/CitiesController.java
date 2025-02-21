3ed0b385b647065253416e3baaacc3510800d968
Joshua Youden <Joshua.Youden@keyin.com>
2025-02-21 16:41:30 -0330

    Finished CitiesController.java

--- jy_mid_sprint_devops_server_api/src/main/java/com/keyin/rest/cities/CitiesController.java
+++ jy_mid_sprint_devops_server_api/src/main/java/com/keyin/rest/cities/CitiesController.java
@@ -1,3 +1,50 @@
 package com.keyin.rest.cities;
 
-public class CitiesController {}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.keyin.rest.cities.Cities;
+import com.keyin.rest.cities.CitiesService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class CitiesController {
+        @Autowired
+    private CitiesService cityService;
+
+    @GetMapping("/cities")
+    public List<Cities> getAllCities() {
+        return cityService.findAllCities();
+    }
+
+    @GetMapping("/cities/{id}")
+    public Cities getCityByID(@PathVariable long id) {
+        return cityService.findCityById(id);
+    }
+
+    @GetMapping("/city_search")
+    public List<Cities> searchCity(@RequestParam(value = "name", required = false) String name) {
+        List<Cities> results = new ArrayList<Cities>();
+
+        Cities city = cityService.findByName(name);
+
+        if (city != null) {
+            results.add(city);
+        }
+
+        return results;
+    }
+
+    @PostMapping("/city")
+    public Cities createCity(@RequestBody Cities newCity) {
+        return cityService.createCity(newCity);
+    }
+
+    @PutMapping("/city/{id}")
+    public Cities updateCity(@PathVariable long id, @RequestBody Cities updatedCity) {
+        return cityService.updateCity(id, updatedCity);
+    }
+}
