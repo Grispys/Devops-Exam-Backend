c6f72fba39c2846b3255364c214113c428d5af5b
Joshua Youden <Joshua.Youden@keyin.com>
2025-02-21 16:32:43 -0330

    Slight alter to name in pom.xml and finished Cities.java

--- jy_mid_sprint_devops_server_api/src/main/java/com/keyin/rest/cities/Cities.java
+++ jy_mid_sprint_devops_server_api/src/main/java/com/keyin/rest/cities/Cities.java
@@ -1,5 +1,50 @@
 package com.keyin.rest.cities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
 public class Cities {
-    
+    @Id
+    @SequenceGenerator(name = "city_sequence", sequenceName = "city_sequence", allocationSize = 1, initialValue=1)
+    @GeneratedValue(generator = "city_sequence")
+    private long id;
+
+    private String name;
+    private String state;
+    private int population;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 }
