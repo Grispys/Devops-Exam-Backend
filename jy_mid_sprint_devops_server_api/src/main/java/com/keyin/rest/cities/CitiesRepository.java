d5e0988282babc4568344ac951bb87bf78352658
Joshua Youden <Joshua.Youden@keyin.com>
2025-02-21 16:44:21 -0330

    Finished CitiesRepository.java

--- jy_mid_sprint_devops_server_api/src/main/java/com/keyin/rest/cities/CitiesRepository.java
+++ jy_mid_sprint_devops_server_api/src/main/java/com/keyin/rest/cities/CitiesRepository.java
@@ -1,5 +1,11 @@
 package com.keyin.rest.cities;
 
-public class CitiesRepository {
-    
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CitiesRepository extends CrudRepository<Cities, Long> {
+    public Cities findByName(String name);
+    public Cities findByState(String state);
+    public Cities findByPopulation(int population);
 }
